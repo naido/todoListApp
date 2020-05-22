a03e0986638156255832a63b779f1e8016814c1a
Nuno <nuno.c.aido@gmail.com>
2020-05-23 00:00:36 +0100

    Read and render Todos List from backend

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 export class Todo {
   constructor(
@@ -20,18 +21,27 @@ export class Todo {
 })
 export class ListTodosComponent implements OnInit {
 
-  todos = [
+  todos: Todo[]
 /*     {id : 1, description : 'Learn to play guitar'},
     {id : 2, description : 'Learn Spring Framework'},
     {id : 3, description : 'Learn to sing'} */
-    new Todo(1, 'Learn to play guitar', false, new Date()),
+
+    /*    new Todo(1, 'Learn to play guitar', false, new Date()),
     new Todo(2, 'Learn Spring Frameworkkk', false, new Date()),
     new Todo(3, 'Learn to sing', false, new Date())
-  ]
+  ]*/
   
-  constructor() { }
+  constructor(
+    private toDoService: TodoDataService
+  ) { }
 
   ngOnInit(): void {
+    this.toDoService.retrieveAllTodos('naido').subscribe(
+        response => {
+          console.log(response);
+          this.todos=response;  
+        }
+    )
   }
 
 }
